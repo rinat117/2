00f3ca3384e5d77b8af117deea0989effd74f236
kabinet6.3@mail.ru <kabinet6.3@mail.ru>
2021-05-17 10:19:54 +0300

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace golovolomka
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // randomizer - eto imya
+        Random randomizer = new Random();
+
+        // sozdanie peremennih
+        int addent1;
+        int addent2;
+
+        // sozdanie peremennih dlya -
+        int minuend;
+        int subtrahend;
+
+        // sozdanie peremennih dlya *
+        int multiplicand;
+        int multiplier;
+
+        // sozdanie peremennih dlya /
+        int dividend;
+        int divisor;
+
+
+
+        // sozdanie timera
+        int timeLeft;
+
+        public void StartTheQuiz()
+        {
+
+            addent1 = randomizer.Next(51);
+            addent2 = randomizer.Next(51);
+
+            plusLeftLabel.Text = addent1.ToString();
+            plusRightLabel.Text = addent2.ToString();
+
+            sum.Value = 0;
+            timeLeft = 10;
+            timeLabel1.Text = "10 seconds";
+            timer1.Start();
+
+
+            minuend = randomizer.Next(1, 101);
+            subtrahend = randomizer.Next(1, minuend);
+
+            minusLeftLabel.Text = minuend.ToString();
+            minusRightLabel.Text = subtrahend.ToString();
+
+            difference.Value = 0;
+
+
+            multiplicand = randomizer.Next(2, 11);
+            multiplier = randomizer.Next(2, 11);
+            timesLeftLabel.Text = multiplicand.ToString();
+            timesRightLabel.Text = multiplier.ToString();
+            product.Value = 0;
+
+            divisor = randomizer.Next(2, 11);
+            int temporaryQuotient = randomizer.Next(2, 11);
+            dividend = divisor * temporaryQuotient;
+            dividedLeftLabel.Text = dividend.ToString();
+            dividedRightLabel.Text = divisor.ToString();
+            quotient.Value = 0;
+
+
+
+            // 10 eto kol-vo sekynd
+            timer1.Start();
+            timeLeft = 30;
+            timeLabel1.Text = "30 seconds";
+          
+
+
+        }
+
+        private bool CheckTheAnswer()
+        {
+            // komp sam skladivaet add1 + add2 i 
+            // proveryat s hashim otvetom v sum
+            if ((addent1 + addent2 == sum.Value)
+
+                // komp sam skladivaet minuend + subtrahend i 
+                // proveryat s hashim otvetom v difference
+                && (minuend - subtrahend == difference.Value)
+
+                // komp sam skladivaet minuend + subtrahend i 
+                // proveryat s hashim otvetom v difference
+                && (multiplicand * multiplier == product.Value)
+
+                // komp sam skladivaet minuend + subtrahend i 
+                // proveryat s hashim otvetom v difference
+                && (dividend / divisor == quotient.Value))
+                return true;
+            else
+                return false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // zapysk programmi
+            StartTheQuiz();
+
+            // chel ne mojet najimat' BUTTON
+            startButton.Enabled = false;
+
+            timeLabel1.BackColor = Color.YellowGreen;
+
+        }
+
+
+        private void answer_enter(object sender, EventArgs e)
+        {
+            NumericUpDown answerBox = sender as NumericUpDown;
+
+            if (answerBox != null)
+            {
+                int lengtOfAnswer = answerBox.Value.ToString().Length;
+                answerBox.Select(0, lengtOfAnswer);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+            {
+                if (CheckTheAnswer())
+                {
+                    timer1.Stop();
+                    MessageBox.Show("Vi vveli vse pravil'no",
+                                          "Pozdravlyuy");
+                    startButton.Enabled = true;
+
+                }
+
+                // esli timeleft bolshe 0 to:
+                else if (timeLeft > 0)
+                {
+                    // таймер минусует по одному (1ой секунде)
+                    // и выводит оставшееся время (число + секунды)
+                    timeLeft--;
+                    timeLabel1.Text = timeLeft + "  seconds ";
+                }
+
+                //// esli timeleft menshe 0 to:
+                else
+
+                {
+
+                    timer1.Stop();
+                    timeLabel1.Text = "Vremya vishlo";
+                    MessageBox.Show("Y tebya chto-to nepravil'no", "Sorry, bro");
+
+                    sum.Value = addent1 + addent2;
+                    difference.Value = minuend - subtrahend;
+                    product.Value = multiplicand * multiplier;
+                    quotient.Value = dividend / divisor;
+
+                    startButton.Enabled = true;
+
+                }
+            }
+
+        }
+    }
---
+++ para/para/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace para
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Inset;
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(534, 511);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label1.Location = new System.Drawing.Point(5, 2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 125);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Form1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 511);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Matching Game";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
